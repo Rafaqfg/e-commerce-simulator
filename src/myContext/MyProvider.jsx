44595582189af8d94585746577d4d52be8e2eb25
Rafael Godoy <rafa.qfg@gmail.com>
2022-07-06 18:18:40 -0300

    FEAT: create context

---
+++ src/myContext/MyProvider.jsx
@@ -0,0 +1,32 @@
+import React, { useState } from 'react';
+import PropTypes from 'prop-types';
+import MyContext from './MyContext';
+
+function MyProvider({ children }) {
+  const [productsList, setProductsList] = useState([]);
+  const [range, setRange] = useState({
+    min: 0,
+    max: 10000,
+  });
+ 
+
+
+  const ContextObject = {
+    productsList,
+    setProductsList,
+    range,
+    setRange,
+  };
+
+  return (
+    <MyContext.Provider value={ ContextObject }>
+      { children }
+    </MyContext.Provider>
+  );
+}
+
+MyProvider.propTypes = {
+  children: PropTypes.node.isRequired,
+};
+
+export default MyProvider;
